9232b111211d3eedf14b789572b1da1f7879079a
Anderson Leite <anderson.leite.bsi@gmail.com>
2021-12-17 08:41:04 -0300

    imp req 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import StarwarsProvider from './context/StarwarsProvider';
 import Table from './components/Table';
 import './App.css';
+import InputFilterPlanets from './components/InputFilterPlanets';
 
 function App() {
   return (
     <StarwarsProvider>
+      <h1>Projeto Star Wars - Trybe</h1>
+      <InputFilterPlanets />
       <Table />
     </StarwarsProvider>
   );

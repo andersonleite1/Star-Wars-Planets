da31475afff4eb2c43ba1c43117208aeaddfec32
Anderson Leite <anderson.leite.bsi@gmail.com>
2021-12-17 08:40:27 -0300

    created component InputFilterPlanets

---
+++ src/components/InputFilterPlanets.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import StarwarsContext from '../context/Context';
+
+export default function InputFilterPlanets() {
+  const { filters, setFilters } = useContext(StarwarsContext);
+  const { filterByName } = filters;
+  const { name } = filterByName;
+
+  const handleChange = ({ target: { value } }) => {
+    setFilters({
+      ...filters,
+      filterByName: {
+        name: value,
+      },
+    });
+  };
+
+  return (
+    <input
+      type="text"
+      name="name"
+      className="input-filter-name"
+      data-testid="name-filter"
+      placeholder="Filter by name"
+      value={ name }
+      onChange={ (event) => handleChange(event) }
+    />
+  );
+}

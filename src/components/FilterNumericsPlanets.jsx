0431af0cda9386e9a819323603687539116c6dcc
Anderson Leite <andersonleite.dev@gmail.com>
2022-03-09 09:58:15 -0300

    feat: add style in component FilterNumericsPlanets

--- src/components/FilterNumericsPlanets.jsx
+++ src/components/FilterNumericsPlanets.jsx
@@ -1,7 +1,65 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import StarwarsContext from '../context/Context';
 
 export default function FilterNumericsPlanets() {
+  const Container = styled.section`
+    margin: 0 2rem;
+  `;
+
+  const Label = styled.label`
+    font-size: 1rem;
+    font-weight: 600;
+    color: white;
+  `;
+
+  const Select = styled.select`
+    height: 1.9rem;
+    margin: 0.2rem;
+    padding: 0 0.2rem;
+    border-radius: 0.2rem;
+    font-size: 0.9rem;
+
+    &:focus {
+      border: solid 2px yellow;
+    }
+
+    &:hover {
+      border-color: yellow;
+    }
+  `;
+
+  const Input = styled.input`
+    margin: 0.2rem;
+    padding: 0 0.5rem;
+    height: 1.6rem;
+    border-radius: 0.2rem;
+    font-size: 0.9rem;
+
+    &:hover {
+      border-color: yellow;
+    }
+  `;
+
+  const Button = styled.button`
+    margin: 0.2rem;
+    padding: 0 0.5rem;
+    width: 5.3rem;
+    height: 1.8rem;
+    border: 1px solid darkgreen;
+    border-radius: 0.2rem;
+    font-size: 1rem;
+    font-weight: 600;
+    color: white;
+    background-color: green;
+    cursor: pointer;
+
+    &:hover {
+      background-color: darkgreen;
+      border-color: yellow;
+    }
+  `;
+
   const {
     column,
     filters,
@@ -28,10 +86,10 @@ export default function FilterNumericsPlanets() {
   };
 
   return (
-    <div>
-      <label htmlFor="column-filter">
+    <Container>
+      <Label htmlFor="column-filter">
         Column
-        <select
+        <Select
           name="column"
           id="column-filter"
           data-testid="column-filter"
@@ -40,11 +98,11 @@ export default function FilterNumericsPlanets() {
           {column.map((data, index) => (
             <option key={ `${data}-${index}` } value={ data }>{data}</option>
           ))}
-        </select>
-      </label>
-      <label htmlFor="comparison-filter">
+        </Select>
+      </Label>
+      <Label htmlFor="comparison-filter">
         Operator
-        <select
+        <Select
           name="comparison"
           id="comparison-filter"
           data-testid="comparison-filter"
@@ -53,22 +111,22 @@ export default function FilterNumericsPlanets() {
           <option value="maior que">maior que</option>
           <option value="menor que">menor que</option>
           <option value="igual a">igual a</option>
-        </select>
-      </label>
-      <input
+        </Select>
+      </Label>
+      <Input
         name="value"
         type="number"
         data-testid="value-filter"
         value={ filters.filterByNumericValues[0].value }
         onChange={ handleChange }
       />
-      <button
+      <Button
         type="button"
         data-testid="button-filter"
         onClick={ handleClick }
       >
         Filter
-      </button>
-    </div>
+      </Button>
+    </Container>
   );
 }
